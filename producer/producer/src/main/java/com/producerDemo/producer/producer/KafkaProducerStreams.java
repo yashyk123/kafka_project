fe79e83785340214e7f86f75991d29654eb8c409
yashyk <7ykore@gmail.com>
2025-10-04 19:05:02 +0530

    add sendRiderStatus function in kafakProducerStream and update yml file

--- producer/producer/src/main/java/com/producerDemo/producer/producer/KafkaProducerStreams.java
+++ producer/producer/src/main/java/com/producerDemo/producer/producer/KafkaProducerStreams.java
@@ -2,17 +2,39 @@ package com.producerDemo.producer.producer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 @Configuration
 public class KafkaProducerStreams {
     @Bean
     public Supplier<RiderLocation> sendRiderLocation(){
+        Random random = new Random();
         return ()-> {
-            RiderLocation location = new RiderLocation("riderYk123",65,88);
+            String riderId = "rider" + random.nextInt(20);
+            RiderLocation location = new RiderLocation(riderId,65,88);
             System.out.println("Sending: " + location.getRiderId());
             return location;
+        };
+    }
+
+    @Bean
+    public Supplier<Message<String>> sendRiderStatus(){
+        Random random = new Random();
+        return ()-> {
+            String riderId = "rider"+ random.nextInt(20);
+            String status = random.nextBoolean() ? "ride started" : "ride compled";
+            System.out.println("Sending: "+ status);
+            return MessageBuilder.withPayload(riderId+ ":" + status)
+                    .setHeader(KafkaHeaders.KEY, riderId.getBytes())
+                    .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_PLAIN)
+                    .build();
         };
     }
 }
